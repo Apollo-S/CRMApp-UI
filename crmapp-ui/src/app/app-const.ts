7534dfae47962587d72c48d06ca644d5915380b6
Apollo-S <apollo.saifullin@gmail.com>
2018-11-07 23:02:01 +0200

    changes in navbar bottom, app-const

--- crmapp-ui/src/app/app-const.ts
+++ crmapp-ui/src/app/app-const.ts
@@ -1,5 +1,8 @@
+import {HttpHeaders} from "@angular/common/http";
+
 export class AppConst {
 
+    headersJSON = new HttpHeaders({'Content-Type': 'application/json'});
     baseUrl = '/api';
     clientsUrl = '/clients';
     agreementsUrl = '/agreements';
