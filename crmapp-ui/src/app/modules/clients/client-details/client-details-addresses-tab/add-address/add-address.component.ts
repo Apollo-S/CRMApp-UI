2de523985facc746c7cb11714c42fe04b6375281
Apollo-S <apollo.saifullin@gmail.com>
2018-12-10 23:06:11 +0200

    fixes in add address controller

--- crmapp-ui/src/app/modules/clients/client-details/client-details-addresses-tab/add-address/add-address.component.ts
+++ crmapp-ui/src/app/modules/clients/client-details/client-details-addresses-tab/add-address/add-address.component.ts
@@ -1,62 +1,50 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { ClientService } from '../../../../../services/client.service';
-import { UtilService } from '../../../../../services/util.service';
-import { ClientAddress } from '../../../../../models/ClientAddress';
-import { Message } from 'primeng/api';
-import { Client } from '../../../../../models/Client';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {ClientService} from '../../../../../services/client.service';
+import {UtilService} from '../../../../../services/util.service';
+import {ClientAddress} from '../../../../../models/ClientAddress';
+import {Message} from 'primeng/api';
+import {Client} from '../../../../../models/Client';
 
 @Component({
-  selector: 'app-add-address',
-  templateUrl: './add-address.component.html',
-  styleUrls: ['./add-address.component.css']
+    selector: 'app-add-address',
+    templateUrl: './add-address.component.html',
+    styleUrls: ['./add-address.component.css']
 })
-export class AddAddressComponent implements OnInit, OnDestroy {
-  private _propertySubscribtion: Subscription;
-  msgs: Message[] = [];
-  address: ClientAddress = {};
-  client: Client = {};  
-  years: string;
-  ru: any;
+export class AddAddressComponent implements OnInit {
+    msgs: Message[] = [];
+    address: ClientAddress = {};
+    client: Client = {};
+    years: string;
+    ru: any;
 
-  constructor(private service: ClientService, 
-              private router: Router) { }
+    constructor(private service: ClientService,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-    this._propertySubscribtion = this.service.property$
-      .subscribe(
-        p => this.client = p
-      );
-    this.ru = UtilService.getCalendarLocalSet();
-    this.years = UtilService.getCalendarYears(5);
-  }
+    ngOnInit() {
+        this.ru = UtilService.getCalendarLocalSet();
+        this.years = UtilService.getCalendarYears(5);
+    }
 
-  onSubmit() {
-    this.save();
-    this.goBackToAddresses();
-  }
+    onSubmit() {
+        this.save();
+        // this.goBackToAddresses();
+    }
 
-  ngOnDestroy(): void {
-    this._propertySubscribtion.unsubscribe();
-  }
+    private save(): void {
+        let msg = '';
+        this.service.addAddress(this.address, this.client)
+            .subscribe(
+                response => {
+                    msg = 'Адрес для ' + this.client.alias + ' успешно добавлен (ID=' + response.id + ')';
+                    this.msgs = [{severity: 'success', summary: 'Успешно', detail: msg}];
+                }
+            );
+    }
 
-  private save(): void {
-    let msg  = '';
-    this.service.addAddress(this.address, this.client)
-      .subscribe(
-        response => {
-          msg = 'Адрес для ' + this.client.alias +  ' успешно добавлен (ID=' + response.id + ')';
-          this.msgs = [{severity:'success', summary:'Успешно', detail: msg}];
-        }
-      );
-  }
-
-  private goBackToAddresses() {
-    setTimeout(
-      (router) => {
+    private goBackToAddresses() {
         this.router.navigate([this.client.url, 'addresses']);
-      }, 1500);
-  }
+    }
 
 }
