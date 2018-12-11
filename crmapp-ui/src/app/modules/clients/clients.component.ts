e223674a25732f5a830cdf84f61fccf535e96eb8
Apollo-S <apollo.saifullin@gmail.com>
2018-12-11 15:09:49 +0200

    added logic for 'refresh' button (@Output event)

--- crmapp-ui/src/app/modules/clients/clients.component.ts
+++ crmapp-ui/src/app/modules/clients/clients.component.ts
@@ -1,51 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { ClientService } from '../../services/client.service';
-import { MenuItem } from 'primeng/api';
+import {Component, OnInit} from '@angular/core';
+import {ClientService} from '../../services/client.service';
+import {MenuItem} from 'primeng/api';
 import {Client} from "../../models/Client";
 
 @Component({
-  selector: 'app-clients',
-  templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+    selector: 'app-clients',
+    templateUrl: './clients.component.html',
+    styleUrls: ['./clients.component.css']
 })
 export class ClientsComponent implements OnInit {
-  columns = [];
-  items: MenuItem[] = [];
-  clients: Client[] = [];
-  loading: boolean = true;
-  
-  constructor(public clientService: ClientService) {
-      clientService.fetchClients().subscribe(
-          clients => {
-              this.clients = clients;
-              this.loading = false;
-          });
-      this.initColumns();
-      this.initMenu();
-  }
+    columns = [];
+    items: MenuItem[] = [];
+    clients: Client[] = [];
+    loading: boolean;
 
-  ngOnInit() {}
+    constructor(public clientService: ClientService) {
+        this.initColumns();
+        this.initMenu();
+    }
 
-  getClients() {
-      return this.clientService.getClients();
-  }
+    ngOnInit() {
+        this.refreshDatasource();
+    }
 
-  private initColumns() {
-    this.columns = [
-      { field: 'id', header: 'ID' },
-      { field: 'alias', header: 'Наименование' },
-      { field: 'title', header: 'Полное наименование' },
-      { field: 'edrpou', header: 'ЕГРПОУ' }
-    ];
-  }
+    refreshDatasource() {
+        this.loading = true;
+        this.clientService.fetchClients().subscribe(
+            clients => {
+                this.clients = clients;
+                this.loading = false;
+            });
+    }
 
-  private initMenu() {
-    this.items = [
-      { label: 'Договоры', icon: 'fa fa-file-text-o', title: 'agreements' },
-      { label: 'Адресы', icon: 'fa fa-building-o', title: 'addresses' },
-      { label: 'Руководители', icon: 'fa fa-user-o', title: 'directors' },
-      { label: 'Банк. реквизиты', icon: 'fa fa-bank', title: 'accounts' }
-    ];
-  }
+    getClients() {
+        return this.clientService.getClients();
+    }
+
+    private initColumns() {
+        this.columns = [
+            {field: 'id', header: 'ID'},
+            {field: 'alias', header: 'Наименование'},
+            {field: 'title', header: 'Полное наименование'},
+            {field: 'edrpou', header: 'ЕГРПОУ'}
+        ];
+    }
+
+    private initMenu() {
+        this.items = [
+            {label: 'Договоры', icon: 'fa fa-file-text-o', title: 'agreements'},
+            {label: 'Адресы', icon: 'fa fa-building-o', title: 'addresses'},
+            {label: 'Руководители', icon: 'fa fa-user-o', title: 'directors'},
+            {label: 'Банк. реквизиты', icon: 'fa fa-bank', title: 'accounts'}
+        ];
+    }
 
 }
--- crmapp-ui/src/app/modules/dashboard/dashboard-table/dashboard-table.component.ts
+++ crmapp-ui/src/app/modules/dashboard/dashboard-table/dashboard-table.component.ts
@@ -1,51 +1,50 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { MenuItem } from 'primeng/api';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Router} from '@angular/router';
+import {MenuItem} from 'primeng/api';
 
 @Component({
-  selector: 'app-dashboard-table',
-  templateUrl: './dashboard-table.component.html',
-  styleUrls: ['./dashboard-table.component.css']
+    selector: 'app-dashboard-table',
+    templateUrl: './dashboard-table.component.html',
+    styleUrls: ['./dashboard-table.component.css']
 })
 export class DashboardTableComponent implements OnInit {
-  
-  @Input() title: string;
-  @Input() columns: Array<any>;
-  @Input() datasource: Array<any>;
-  @Input() responsive: boolean;
-  @Input() reorderableColumns: boolean; 
-  @Input() rowHover: boolean; 
-  @Input() paginator: boolean; 
-  @Input() rows: number; 
-  @Input() loading: boolean; 
-  @Input() menuModel: Array<MenuItem>; 
-  @Input() sortField: string;
-  @Input() autoLayout: boolean;
-  @Input() addButtonTitle: string;
-  @Input() editButtonTitle: string;
-  @Input() additionalButtonTitle: string;
-  selectedItem: any;
 
-  constructor(private router: Router) { }
+    @Input() title: string;
+    @Input() columns: Array<any>;
+    @Input() datasource: Array<any>;
+    @Input() responsive: boolean;
+    @Input() reorderableColumns: boolean;
+    @Input() rowHover: boolean;
+    @Input() paginator: boolean;
+    @Input() rows: number;
+    @Input() loading: boolean;
+    @Input() menuModel: Array<MenuItem>;
+    @Input() sortField: string;
+    @Input() autoLayout: boolean;
+    @Input() addButtonTitle: string;
+    @Input() editButtonTitle: string;
+    @Input() additionalButtonTitle: string;
+    @Output() refreshDatasource: EventEmitter<any> = new EventEmitter();
+    selectedItem: any;
 
-  ngOnInit() {
-    setTimeout(() => {
-      this.loading = false;
-    }, 700);
-  }
+    constructor(private router: Router) {
+    }
 
-  goToEntry(url: string) {
-    this.router.navigate([url]);
-  }
+    ngOnInit() {
+    }
 
-  initMenu(routerLinkUrl) {
-    this.menuModel.forEach(
-      (menu) => menu.routerLink = [routerLinkUrl, menu.title]
-    );
-  }
+    goToEntry(url: string) {
+        this.router.navigate([url]);
+    }
 
-  refresh() {
-  //TODO implement refreshing of table (new request)
-  }
+    initMenu(routerLinkUrl) {
+        this.menuModel.forEach(
+            (menu) => menu.routerLink = [routerLinkUrl, menu.title]
+        );
+    }
+
+    refresh() {
+        this.refreshDatasource.emit(true);
+    }
 
 }
