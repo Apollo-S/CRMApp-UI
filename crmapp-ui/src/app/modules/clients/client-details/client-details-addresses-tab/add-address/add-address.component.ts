1a9b32f4f414d0e66d1e911e5814b6130274fc06
Apollo-S <apollo.saifullin@gmail.com>
2018-12-17 15:47:19 +0200

    fixed error messages on add address form, controller fixes (usage of reactive forms)

--- crmapp-ui/src/app/modules/clients/client-details/client-details-addresses-tab/add-address/add-address.component.ts
+++ crmapp-ui/src/app/modules/clients/client-details/client-details-addresses-tab/add-address/add-address.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
 import {ClientService} from '../../../../../services/client.service';
 import {UtilService} from '../../../../../services/util.service';
 import {ClientAddress} from '../../../../../models/ClientAddress';
-import {Message} from 'primeng/api';
-import {Client} from '../../../../../models/Client';
+import {MessageService} from 'primeng/api';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-add-address',
@@ -12,39 +11,73 @@ import {Client} from '../../../../../models/Client';
     styleUrls: ['./add-address.component.css']
 })
 export class AddAddressComponent implements OnInit {
-    msgs: Message[] = [];
-    address: ClientAddress = {};
-    client: Client = {};
     years: string;
     ru: any;
+    addressForm: FormGroup;
 
-    constructor(private service: ClientService,
-                private router: Router) {
-    }
-
-    ngOnInit() {
+    constructor(private clientService: ClientService,
+                private formBuilder: FormBuilder,
+                private messageService: MessageService) {
         this.ru = UtilService.getCalendarLocalSet();
         this.years = UtilService.getCalendarYears(5);
     }
 
-    onSubmit() {
-        this.save();
-        // this.goBackToAddresses();
+    ngOnInit() {
+        this.initAddressForm();
     }
 
-    private save(): void {
-        let msg = '';
-        this.service.addAddress(this.address, this.client)
-            .subscribe(
-                response => {
-                    msg = 'Адрес для ' + this.client.alias + ' успешно добавлен (ID=' + response.id + ')';
-                    this.msgs = [{severity: 'success', summary: 'Успешно', detail: msg}];
-                }
-            );
+    onSubmit() {
+        this.save();
+    }
+
+    private initAddressForm() {
+        this.addressForm = this.formBuilder.group({
+            presentation: ['', Validators.compose([
+                Validators.required
+            ])],
+            dateStart: ['', Validators.compose([
+                Validators.required,
+                Validators.nullValidator
+            ])]
+        });
+    }
+
+    getClient() {
+        return this.clientService.getCurrentClient();
+    }
+
+    private save() {
+        let msg = 'Адрес для ' + this.getClient().alias;
+        let address: ClientAddress = new ClientAddress();
+        address.presentation = this.addressForm.controls.presentation.value;
+        address.dateStart = this.addressForm.controls.dateStart.value;
+        this.clientService.addAddress(address, this.getClient().id).toPromise()
+            .then(response => {
+                this.clientService.fetchAllClientDataPromise(this.getClient().id)
+                    .then(() => {
+                        this.messageService.add({
+                            severity: 'success',
+                            summary: 'Успешно!',
+                            detail: msg + ' успешно добавлен (ID=' + response.id + ')'
+                        });
+                    })
+                    .then(() => {
+                        this.goBackToAddresses();
+                    });
+            })
+            .catch(() => {
+                this.messageService.add({
+                    severity: 'error',
+                    summary: 'Ошибка!',
+                    detail: msg + ' не добавлен'
+                });
+            });
     }
 
     private goBackToAddresses() {
-        this.router.navigate([this.client.url, 'addresses']);
+        this.clientService.goToUrl([this.getClient().url, 'addresses'])
+            .then(() => {
+            });
     }
 
 }
