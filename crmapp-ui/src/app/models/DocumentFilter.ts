01184744ada87dbf50d82219d64958ddc8336ac8
Apollo-S <apollo.saifullin@gmail.com>
2019-01-18 23:20:55 +0200

    doc filter interface added

---
+++ crmapp-ui/src/app/models/DocumentFilter.ts
@@ -0,0 +1,8 @@
+export interface DocumentFilter {
+    filterState: boolean;
+    selectedDocTypes: any[];
+    selectedDocStatuses: any[];
+    selectedClients: any[];
+    selectedSortType: string;
+    selectedSortField: any;
+}
