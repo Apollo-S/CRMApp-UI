01a6842d3702f5403fb0ef1a7659de461259ae6a
Apollo-S <apollo.saifullin@gmail.com>
2019-04-09 16:32:27 +0300

    moved commented code and marked model as 'abstract'

--- crmapp-ui/src/app/models/Address.ts
+++ crmapp-ui/src/app/models/Address.ts
@@ -1,20 +1,6 @@
-// export interface Address {
-//     id: number;
-//     country: Object;
-//     region: string;
-//     city: string;
-//     street: string;
-//     building: string;
-//     apartment: string;
-//     zip: string;
-//     presentation: string;
-//     dateStart: Date;
-//     url: string
-// }
-
 import {BaseModel} from "./BaseModel";
 
-export class Address extends BaseModel {
+export abstract class Address extends BaseModel {
     country?: Object;
     region?: string;
     city?: string;
