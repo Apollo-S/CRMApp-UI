accf7ecae9b06cf25205e7b0c58ef18c3b010d6a
Apollo-S <apollo.saifullin@gmail.com>
2019-04-26 23:29:44 +0300

    Fixed: Fetching all documents is getting server error (documents tab in agreement details)

--- crmapp-ui/src/app/modules/agreements/agreement-details/agreement-details-documents-tab/agreement-details-documents-tab.component.ts
+++ crmapp-ui/src/app/modules/agreements/agreement-details/agreement-details-documents-tab/agreement-details-documents-tab.component.ts
@@ -1,55 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { ClientAgreement } from '../../../../models/ClientAgreement';
-import { AgreementService } from '../../../../services/agreement.service';
-import { Document } from '../../../../models/Document';
-import { DocumentService } from '../../../../services/document.service';
-import { DocumentTypeService } from '../../../../services/document-type.service';
-import { DocumentStatusService } from '../../../../services/document-status.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ClientAgreement} from 'app/models/ClientAgreement';
+import {AgreementService} from 'app/services/agreement.service';
+import {Document} from 'app/models/Document';
+import {DocumentService} from 'app/services/document.service';
+import {DocumentTypeService} from 'app/services/document-type.service';
+import {DocumentStatusService} from 'app/services/document-status.service';
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'app-agreement-details-documents-tab',
-  templateUrl: './agreement-details-documents-tab.component.html',
-  styleUrls: ['./agreement-details-documents-tab.component.css']
+    selector: 'app-agreement-details-documents-tab',
+    templateUrl: './agreement-details-documents-tab.component.html',
+    styleUrls: ['./agreement-details-documents-tab.component.css']
 })
-export class AgreementDetailsDocumentsTabComponent implements OnInit {
-  agreement: ClientAgreement;
-  documents: Document[] = [];
-  columns: any[] = [];
+export class AgreementDetailsDocumentsTabComponent implements OnInit, OnDestroy {
+    private subscription: Subscription = new Subscription();
+    agreement: ClientAgreement = new ClientAgreement();
+    documents: Document[] = [];
+    columns: any[] = [];
+    loadingState: boolean;
 
     constructor(private agreementService: AgreementService,
                 private docService: DocumentService,
                 private docTypeService: DocumentTypeService,
                 private docStatusService: DocumentStatusService) {
-        // this.agreement = service.getAgreement();
-        agreementService.emitterAgreement.subscribe(
-            agreement => {
-                this.agreement = agreement;
-                this.getDocumentsByAgreementId(agreement.id);
-            }
-        )
+        this.initColumns();
     }
 
-  ngOnInit() { 
-    this.initColumns();
-  }
+    ngOnInit() {
+        this.loadingState = true;
+        this.subscription = this.agreementService.getCurrentAgreement()
+            .subscribe(agreement => {
+                this.agreement = agreement;
+                this.agreementService.getDocumentsByAgreementId(agreement.id)
+                    .subscribe(documents => {
+                        this.documents = documents;
+                        this.loadingState = false;
+                    });
+            });
+    }
 
-  private getDocumentsByAgreementId(agreementId: number) {
-    this.agreementService.getDocumentsByAgreementId(agreementId)
-      .subscribe(
-        documents => this.documents = documents
-      );
-  }
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 
-  private initColumns() {
-    this.columns = [
-      { field: 'id', header: 'ID', colStyle: 'text-align:center' },
-      { field: 'docType.shortTitle', header: 'Тип документа', colStyle: 'text-align:center' },
-      { field: 'number', header: '№', colStyle: 'text-align:center' },
-      { field: 'amount', header: 'Сумма', colStyle: 'text-align:center' },
-      { field: 'dated', header: 'Дата', colStyle: 'text-align:center' },
-      { field: 'paymentDate', header: 'Дата оплаты', colStyle: 'paid' },
-      { field: 'status.status', header: 'Статус', colStyle: 'text-align:center' }
-    ];
-  }
+    private initColumns() {
+        this.columns = [
+            {field: 'id', header: 'ID', colStyle: 'text-align:center'},
+            {field: 'docType.shortTitle', header: 'Тип документа', colStyle: 'text-align:center'},
+            {field: 'number', header: '№', colStyle: 'text-align:center'},
+            {field: 'amount', header: 'Сумма', colStyle: 'text-align:center'},
+            {field: 'dated', header: 'Дата', colStyle: 'text-align:center'},
+            {field: 'paymentDate', header: 'Дата оплаты', colStyle: 'paid'},
+            {field: 'status.status', header: 'Статус', colStyle: 'text-align:center'}
+        ];
+    }
 
 }
